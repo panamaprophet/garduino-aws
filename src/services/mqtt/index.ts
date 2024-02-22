45978ab90c0fcd115c2544a047297a40feec6dcd
pp <pp@rawsound.ru>
2024-02-22 13:50:55 +0100

    fix typo

--- src/services/mqtt/index.ts
+++ src/services/mqtt/index.ts
@@ -1,18 +1,18 @@
 import { handler as mqttConfigurationTopicHandler } from './topics/configuration/pub';
-import { handler as mqttEventTopicHandler } from './topics/event/pub';
+import { handler as mqttEventTopicHandler } from './topics/events/pub';
 import { handler as mqttRebootTopicHandler } from './topics/reboot/pub';
 import { handler as mqttStatusTopicHandler } from './topics/status/pub';
 
 
-type MessageType = 'config' | 'event' | 'reboot' | 'status';
+type MessageType = 'config' | 'events' | 'reboot' | 'status';
 
-type MessagePayload = { controllerId: string; messageType: MessageType;[k: string]: unknown };
+type MessagePayload = { controllerId: string; messageType: MessageType; [k: string]: unknown };
 
 type MessageHandler = (payload: MessagePayload) => Promise<void>;
 
 
 const messageTypeToHandlerMap: { [k in MessageType]: MessageHandler } = {
-    'event': mqttEventTopicHandler,
+    'events': mqttEventTopicHandler,
     'reboot': mqttRebootTopicHandler,
     'status': mqttStatusTopicHandler,
     'config': mqttConfigurationTopicHandler,
